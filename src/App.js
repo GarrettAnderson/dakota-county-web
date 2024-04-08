2d223700c56c5ebb654d8d025181f1e989847ae5
Garrett <garrettleegrahamanderson@gmail.com>
2024-04-08 20:19:51 +0000

    added preliminary html for form in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const getCaseRecords = (name, ssn, caseNum) => {
+    console.log(
+      "Get records from - Include Case #, SSN and DOB as input criteria"
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Set Up Custom Connector</h1>
+        <h6>
+          Add credentials to set up a Custom Connector. For details on usage,
+          please check the <a href="#">documentation</a>.
+        </h6>
       </header>
+
+      <form action={getCaseRecords}>
+        <label htmlFor="client-secret"></label>{" "}
+        {/* Is textarea within label similart to inputs?? */}
+        <textarea name="client-secret" rows={4} cols={40} />
+        <br />
+        <button
+        // formAction={save}
+        >
+          Cancel
+        </button>
+        <button type="submit" name="button" value="submit">
+          Done
+        </button>
+      </form>
     </div>
   );
 }
