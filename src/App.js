455a407d37a7572e0e97454e98de8856619c60be
Garrett <garrettleegrahamanderson@gmail.com>
2024-04-09 03:34:36 +0000

    created form with material UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import logo from "./logo.svg";
+import * as React from "react";
+import FormControl, { useFormControl } from "@mui/material/FormControl";
+import {
+  FormHelperText,
+  Input,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
 import "./App.css";
 
 function App() {
@@ -18,6 +25,48 @@ function App() {
       </header>
 
       <form action={getCaseRecords}>
+        <FormControl required>
+          <InputLabel htmlFor="name" disableAnimation={true}>
+            Name
+          </InputLabel>
+          <OutlinedInput id="name" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">
+            Provide unique name for the connector
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="name">Authentication Type</InputLabel>
+          {/* Select dropdown of options - OAuth 2.0 */}
+        </FormControl>
+        <FormControl required>
+          <InputLabel htmlFor="resource-domain">Resource Domain</InputLabel>
+          <OutlinedInput
+            id="resource-domain"
+            aria-describedby="my-helper-text"
+            placeholder="https://"
+          />
+          <FormHelperText id="my-helper-text">
+            Domain that will be used to access data
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="grant-type">Grant Type</InputLabel>
+          {/* Select dropdown of options - Password Grant */}
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="username">User Name</InputLabel>
+          <OutlinedInput id="username" variant="outlined" />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="password" disableAnimation={true}>
+            Password
+          </InputLabel>
+          <OutlinedInput id="password" variant="outlined" />
+        </FormControl>
+        <FormControl required>
+          <InputLabel htmlFor="client-id">Client ID</InputLabel>
+          <OutlinedInput id="client-id" variant="outlined" />
+        </FormControl>
         <label htmlFor="client-secret"></label>{" "}
         {/* Is textarea within label similart to inputs?? */}
         <textarea name="client-secret" rows={4} cols={40} />
